8953999f567d58440f3281b7e951c5759a6ab75d
KevinHa48 <kh220kh@gmail.com>
2021-04-03 17:25:27 -0400

    added mongodb to bot

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 const Discord = require("discord.js");
 const helpmsg = require("./helpmessage.json")
 const qbank = require("./triviaquestions.json")
+const mongo = require('./mongo')
 //const cron = require('cron');
 
 require('dotenv').config();
@@ -14,30 +15,34 @@ require('dotenv').config();
 const client = new Discord.Client();
 const prefix = "!";
 var i = 0;
+var controller;
+var join_status = true;
+var current_answer;
+var answered = false;
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log("Trivia Bot is now online!"); 
 
-    //Sends question every 5 seconds, put it in ready for now to test.
-    //require converts json file to JS object already, just need to parse it for it's question and filename.
-    // var q = setInterval(() => {
-    //   client.channels.cache.get('826315775111200848').send(`Question: ${qbank.trivia_bank[i].question}`, 
-    //   {files: [`${qbank.trivia_bank[i].image}`]});
-    //   i++;
-    //   if(i == qbank.trivia_bank.length) {clearInterval(q)};
-    // }, 5000);
-
+    await mongo().then(mongoose => {
+      try {
+        console.log('Successfully connected to mongo!');
+      } finally {
+        mongoose.connection.close();
+      }
+    })
+    
 });
 
 
 function sendTrivia() {
   if(i == qbank.trivia_bank.length) {
-    clearInterval(q);
+    return;
   }
   else {
-    i++;
     client.channels.cache.get('826315775111200848').send(`Question: ${qbank.trivia_bank[i].question}`, 
     {files: [`${qbank.trivia_bank[i].image}`]});
+    current_answer = qbank.trivia_bank[i].answer;
+    i++;
   }
   
 }
@@ -50,13 +55,33 @@ function sendTrivia() {
 
 client.on("message", message => {
     if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+
+    if (!message.content.startsWith(prefix)) {
+      if(message.channel.type === 'dm' && join_status && !answered) {
+        console.log("User attempted to answer question.")
+        if(message.content == current_answer) {
+          message.author.send(`Correct!`);
+          answered = true;
+        } else {
+          message.author.send(`Incorrect, try again.`);
+        }
+      }
+      else if (answered) {
+        message.author.send(`You already answered correctly, sit tight for the next question!`)
+        return;
+      }
+      else {
+        message.author.send(`It looks like you haven't joined using the !join command.`);
+        return;
+      }
+    }
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
     if (command === "help") {
+      setTimeout(() => message.delete(), 3000);
       message.author.send({ embed: helpmsg });
     }
 
@@ -66,11 +91,17 @@ client.on("message", message => {
       var perm_check = message.member.roles.cache.has('827662867171901481');
 
       if (perm_check && (command === "start") ) {
-        controller = setInterval(sendTrivia, 5000);
+        message.delete();
+        console.log("Warning: Used an admin command!!!");
+        controller = setInterval(sendTrivia, 20000);
+        console.log("Started trivia controller.");
       }
   
       if (perm_check && (command === "stop") ) {
+        message.delete();
         console.log("Warning: Used an admin command!!!");
+        clearInterval(controller);
+        console.log("Stopped trivia controller.");
       }
     }
     
@@ -78,4 +109,5 @@ client.on("message", message => {
     
 
 });
+
 client.login(process.env.TOKEN);
