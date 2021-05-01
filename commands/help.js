6326f69d50deb406b35ed70b1524c42989c0c928
KevinHa48 <kh220kh@gmail.com>
2021-05-01 01:01:09 -0400

    a

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'help',
     description: 'Simple help command.',
     execute(message, args) {
-        const date = Date.now();
+        const date = new Date();
         if(date.getDay() == 6) {
             console.log('e');
         }
